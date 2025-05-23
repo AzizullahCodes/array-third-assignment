401dc051d866227931f907c2697802897287a95c
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-23 10:27:58 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,189 @@
+// array practic third assignment 
+// 🧠 Assignment 1: Add and Remove Items
+// Task:
+// Create an empty array.
+
+// Add "apple", "banana" and "mango".
+
+// Remove the last item.
+
+// Add "grapes" at the beginning .
+
+// Remove the first item.
+
+// Expected Output: Show the final array in the console.
+
+// ans = 
+        // var emptyArray = [];
+        // emptyArray.push('apple');
+        // emptyArray.push('banana');
+        // emptyArray.push('mango'); 
+        // console.log(emptyArray);
+        // emptyArray.pop();
+        // console.log(emptyArray);
+        // emptyArray.unshift('grapes');
+        // console.log(emptyArray); 
+        // emptyArray.shift();
+        // console.log(emptyArray);
+
+//         // 🧠 Assignment 2: Access Specific Items
+// Task:
+// Create an array ["red", "green", "blue", "yellow"]
+
+// Access and print the first, second, and last elements using their indexes.
+    // var array = ['red','green','blue','yellow'];
+    // var print = array[0] + ' ' +  array[1] + ' ' + array[3];
+    // console.log(print)
+// ans
+// var array = ['red','green','blue','yellow'];
+// var print = array[1] + ' ' + array[2] + ' ' + array[3];
+// console.log(print); 
+// 
+// // 🧠 Assignment 3: display second 
+// var fruits = ['apple','mango','orange','guava'];
+// alert('the scond fruit is : ' + fruits[1]); 
+
+// // 🧠 Assignment 4:push method 
+                // var fruits = ['apple','mango','orange','guava']; 
+                // fruits.push('lemon');
+                // console.log(fruits);
+
+                //  var fruits = ['apple','mango','orange','guava'];
+                
+                // // 🧠 Assignment 5:shift method
+                //  var fruits = ['apple','mango','orange','guava']; 
+                //  fruits.shift();
+                //  console.log(fruits); 
+
+                // // 🧠 Assignment 6: shift method 
+                // var fruits = ['apple','mango','orange','guava']; 
+                // fruits.shift();
+                // console.log(fruits); 
+// // 🧠 Assignment 7:splice method 
+                // var fruits = ['apple','mango','orange','guava'];
+                // fruits.splice(3,1,'banana')
+                // console.log(fruits); 
+
+                // // 🧠 Assignment 8:reverse method 
+                //  var fruits = ['apple','mango','orange','guava'];
+                //  fruits.reverse();
+                //  console.log(fruits); 
+                
+                 // // 🧠 Assignment 9:search ; 
+                //  var fruits = ['apple','mango','orange','guava']; 
+                //  var check = prompt('ente any fruits you want to check in array ');
+                //  if (check == fruits[0]|| 
+                //         check == fruits[1]|| 
+                //         check == fruits[2]||
+                //         check == fruits[3]
+                //  ){console.log('your searching fruit is available');}
+                //  else{console.log('not available in list');}
+
+
+// // 🧠 Assignment 10:check if last index is reverse, if is reverse than reverse all array ; 
+        // var array = ['apple','mango','grapes','reverse'];
+        // if(array[3] === 'reverse'){var newArray = array.reverse();
+        //         console.log(newArray)
+        // }
+        // else{console.log('not')} 
+
+        // // 🧠 Assignment 11: check middle value; 
+                // var array = ['good','ok','excellent'];
+                // if(array[1] == 'ok'){console.log('center ok');}
+                // else{console.log('check center')} 
+
+
+                //  // // 🧠 Assignment 12: check middle value; 
+                //  var array = [];
+                //  if (array[0){console.log('empty')  
+                //  }
+                //  else{console.log('not empty')}
+
+                 // // 🧠 Assignment 13:shift unshift; 
+                        // var array = ['old','young'];
+                        // if (array[0] === 'old'){array.shift() && array.unshift('children')
+                        //         console.log(array)
+                        // }
+                        // else{console.log('first index is not old')} 
+
+                        //  // // 🧠 Assignment 14:if else chain; 
+                                        // var array = ['green','yellow','blue'];
+                                        // if(array[1] == 'red'){console.log('stop');}
+                                        // else if(array[1] == 'yellow'){console.log('wait');}
+                                        // else if(array[1] == 'green'){console.log('go');}
+                                        // else{console.log('the second index not match')}
+
+                                        //  // // 🧠 Assignment 15:indexof();
+                        //                 var fruits = ['apple','banana','orange'];
+                        //                 if(fruits[0] == 'banana' ||
+                        //         fruits[1] == 'banana' ||
+                        // fruits[0] == 'banana'){console.log('available')}
+                        // else{console.log('not available')} 
+
+
+                        // //  // // 🧠 Assignment 16:check number and greater than 100; 
+                //         var mixed = [200,'green',false,'#@sfs'];
+                //         var datatype = typeof(mixed[0]);
+                //         console.log(datatype);
+                //         if(datatype === 'number'){if(mixed[0] > 100){console.log('big number');}
+                // else{console.log('small number');}}
+                // else{console.log('not number');} 
+
+                // //  // // 🧠 Assignment 17 : check first and last match 
+                // var array = [21,67,5,21];
+                // if(array[0] === array[3] && array[3] === array[0]){console.log('same');}
+                // else{console.log('not same');} 
+
+                // //  // // 🧠 Assignment 18:check array length;
+                // var girls = ['knval','klsoom','eman'];
+                // var lengt = girls.length;
+                // console.log(lengt);
+                // if(lengt === 3){console.log('enough length');}
+                // else{console.log('add more length is not enough')}
+
+
+        // //  // // 🧠 Assignment 19:if nested if challenges;
+        //  var fruits = ['apple','orange','guava'];
+        //  if(fruits[0] === 'apple'){console.log('fruit found');}
+        //  else{console.log('fruit not found');}
+
+        // //  // // 🧠 Assignment 20:remove all items from array by pop method;
+        // var animals = ['cat','dog','cow','goat'];
+        // console.log(animals);
+        // animals.pop();
+        // animals.pop();
+        // animals.pop();
+        // animals.pop();
+        // console.log(animals); 
+
+         // //  // // 🧠 Assignment 21:remove all items from array by shift method; 
+        //  var animals = ['cat','dog','cow','goat']; 
+        //  console.log(animals);
+        //  var remove = animals.shift();
+        //  var remove = animals.shift();
+        //  var remove = animals.shift();
+        //  var remove = animals.shift();
+        //  console.log(animals);
+         
+        // //  // // 🧠 Assignment 22:remove all items from array by shift method;
+        // var animals = ['cat','dog','cow','goat'];
+        // var rev = animals.reverse();
+        // console.log(rev);
+        // var chekIndex = animals.indexOf('cow');
+        // console.log(chekIndex);
+
+        // 🧠 Assignment 23:splice method;
+        // var animals = ['cat','dog','hen','cow','goat'];
+        // animals.splice(2,1);
+        // console.log(animals); 
+
+
+        // 🧠 Assignment 24:splice method;
+        //  var animals = ['cat','dog','cow','goat'];
+        //  var newlist = animals.slice();
+        //  console.log(newlist);
+
+
+         // 🧠 Assignment 25:splice method; 
+var animals = ['cat','dog','cow','goat'];
+var find = animals.indexOf('dog')
